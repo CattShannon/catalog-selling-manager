949c76f18cd95057b229ec2b8a4ef437e4db4b3b
Sebastian Aguirre <sebastian.aguirre@sofka.com.co>
2021-06-12 22:29:52 -0500

    test assertion improvement

--- src/test/java/org/sofka/config/ConfigResourceTest.java
+++ src/test/java/org/sofka/config/ConfigResourceTest.java
@@ -1,6 +1,8 @@
 package org.sofka.config;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,11 +13,12 @@ public class ConfigResourceTest {
 
     @Test
     public void test(){
-        given()
-                .when().get("/config/supersonic")
-                .then()
-                .statusCode(200)
-                .body(is("Mach 3 is 2301,811 mph"));
+        Response response = given()
+                .when().get("/config/supersonic");
+
+         response.then().statusCode(200);
+
+         Assertions.assertEquals("Mach 3 is 2301,811 mph", response.asString());
 
     }
 
